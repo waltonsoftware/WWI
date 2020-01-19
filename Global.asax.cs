fa248ddb423fc5f42ec6a4bd49cf94f3976503cb
Mark Walton <mark@waltonsoftware.com>
2020-01-19 16:53:27 -0500

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using WWI.Log4NetExtension;
+
+namespace WWI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+			LoggerInitializer.Initialize();
+			Logger.LogInfo("Site WWI starting");
+
+			AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
