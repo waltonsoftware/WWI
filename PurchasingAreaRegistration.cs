0529fc8371aff868144ee21e3ca7f197ad7152e0
Mark Walton <mark@waltonsoftware.com>
2020-01-18 22:18:39 -0500

    Add files via upload

---
+++ PurchasingAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WWI.Areas.Purchasing
+{
+    public class PurchasingAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Purchasing";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Purchasing_default",
+                "Purchasing/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
